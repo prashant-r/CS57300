38dac596f2420d92de8b8191cc0e2bc3ef62f7e8
prashant-r <prashantr186@yahoo.com>
2017-04-23 01:29:34 -0400

    Progress so far

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -2,14 +2,36 @@ import sys
 import os
 import math
 import random
-from sklearn import datasets
+import matplotlib
+#matplotlib.use('Agg')
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
 
-# Import helper functions
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, dir_path + "/../utils")
-from data_manipulation import normalize
-from data_operation import euclidean_distance
+from sklearn.metrics import silhouette_score
+# Calculate the covariance matrix for the dataset X
+def calculate_covariance_matrix(X, Y=np.empty((0,0))):
+    if not Y.any():
+        Y = X
+    n_samples = np.shape(X)[0]
+    covariance_matrix = (1 / (n_samples-1)) * (X - X.mean(0)).T.dot(Y - Y.mean(0))
+    return np.array(covariance_matrix, dtype=float)
+
+# Normalize the dataset X
+def normalize(X, axis=-1, order=2):
+    l2 = np.atleast_1d(np.linalg.norm(X, order, axis))
+    l2[l2 == 0] = 1
+    return X / np.expand_dims(l2, axis)
+
+# Calculate the distance between two vectors
+def euclidean_distance(x1, x2):
+    distance = 0
+    for i in range(len(x1)):
+        distance += pow((x1[i] - x2[i]), 2)
+
+    return math.sqrt(distance)
+
 
 class PCA():
     """A method for doing dimensionality reduction by transforming the feature
@@ -82,24 +104,11 @@ class PCA():
         # Axis labels
         plt.xlabel('Principal Component 1')
         plt.ylabel('Principal Component 2')
-
-        plt.show()
-
-    # Plot the dataset X and the corresponding labels y in 3D using PCA.
-    def plot_in_3d(self, X, y=None):
-        X_transformed = self.transform(X, n_components=3)
-        x1 = X_transformed[:, 0]
-        x2 = X_transformed[:, 1]
-        x3 = X_transformed[:, 2]
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(x1, x2, x3, c=y)
-        plt.show()
-
+        plt.savefig(str(title) + str(".png"))
 
 
 class KMeans():
-    def __init__(self, k=2, max_iterations=500):
+    def __init__(self, k=2, max_iterations=50):
         self.k = k
         self.max_iterations = max_iterations
 
@@ -155,7 +164,7 @@ class KMeans():
     def predict(self, X):
         # Initialize centroids
         centroids = self._init_random_centroids(X)
-
+        result = [];
         # Iterate until convergence or for max iterations
         for _ in range(self.max_iterations):
             # Assign samples to closest centroids (create clusters)
@@ -169,22 +178,60 @@ class KMeans():
             if not diff.any():
                 break
 
-        return self._get_cluster_labels(clusters, X)
+        result.append(self._get_cluster_labels(clusters, X))
+
+
+        return result;
 
 
 def main():
-    # Load the dataset
-    X, y = datasets.make_blobs()
+    if len(sys.argv) >= 3:
+        train_data_file = sys.argv[1]
+        K = sys.argv[2]
+        analysis = sys.argv[3]
+        if analysis == "A1" :
+            print("Performing Analysis A1")
+            images = np.genfromtxt('digits-raw.csv', delimiter=',')
+            getrandom = np.random.randint(images.shape[0])
+            image = images[getrandom][2:]
+            reshaped_image = image.reshape(28, 28)
+            fig, ax = plt.subplots()
+            ax.imshow(image, cmap='gray')
+            plt.title("Analysis1");
+            plt.show()
+        elif analysis == "A2" :
+            print("Performing Analysis A2")
+            emb = np.genfromtxt('digits-embedding.csv', delimiter=',')
+            perm = np.random.permutation(emb.shape[0])[:1000]
+            labels = np.array(emb[perm, 1], dtype=int)
+            colors = np.random.rand(10, 3)[labels, :]
+            fig, ax = plt.subplots()
+            plt.scatter(emb[perm, 2], emb[perm, 3], c=colors, alpha=0.9, s=10)
+            plt.title("Analysis2");
+            plt.savefig(str(plt.title))
+        elif analysis == "B1" :
+            K = [2, 4, 6, 8, 16, 32]
+            fnames = ['digits-embedding.csv']
+            #wc_ssd = np.zeros((len(fnames), len(K)))
+            #sili_sc = np.zeros((len(fnames), len(K)))
+            for i, fname in enumerate(fnames):  
+                X = np.genfromtxt(fname, delimiter=',')[:, 2:]
+                y = np.genfromtxt(fname, delimiter=',')[:, 1]
+                for j, k in enumerate(K):
+                    kmeans = KMeans(k=k,max_iterations= 50)
+                     # Cluster the data using K-Means
+                    y_pred = kmeans.predict(X)
+                    print(y_pred)
+                    pca = PCA()
+                    pca.plot_in_2d(X, y_pred[0], title= str("K=") + str(k) + str("-Means Clustering"))
+                    print("sklearn SC =", silhouette_score(X, y_pred[0]))
 
-    # Cluster the data using K-Means
-    clf = KMeans(k=3)
-    y_pred = clf.predict(X)
-
-    # Project the data onto the 2 primary principal components
-    pca = PCA()
-    pca.plot_in_2d(X, y_pred, title="K-Means Clustering")
-    pca.plot_in_2d(X, y, title="Actual Clustering")
-
+            pca.plot_in_2d(X, y, title="Actual Clustering")
+            #plot_wc_ssd(wc_ssd);
+            #plot_silhouette_score(sili_sc);
+    else:
+        print('usage: python hw5.py dataFileName K')
+        sys.exit(-1)
 
 if __name__ == "__main__":
     main()
